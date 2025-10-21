0e747719a95d5ada4b9099ede41633668ddac3dc
jtahun <jtahun@bk.ru>
2025-10-21 11:10:50 +0500

    Add files via upload
    started with main logic

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE='vocab-json-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./words.json'];
+
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',e=>{
+  const url = new URL(e.request.url);
+  if(url.pathname.endsWith('/words.json') || url.pathname.endsWith('words.json')){
+    e.respondWith(fetch(e.request, {cache:'no-cache'}).then(res=>{
+      const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request, copy)); return res;
+    }).catch(()=>caches.match(e.request)));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+    const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request, copy)); return res;
+  })));
+});
