6b813b375aa699e8058d058dcda868752e48253d
igor.zhuley <jtahun@bk.ru>
2025-11-26 00:39:20 +0500

    added new words section

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='vocab-json-v7.51';
+const CACHE='vocab-json-v7.54';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install',e=>{
